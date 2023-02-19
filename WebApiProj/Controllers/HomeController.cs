c6ed7367cf28df6f3ae532c88dc788617aafaa48
DESKTOP-L3QDU7O\windows 10 pro <shubhamboghara8686@gmail.com>
2023-02-20 00:56:45 +0530

    Creating Api Controller

---
+++ WebApiProj/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiProj.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Ok Works Fine :)");
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok("Ticket Result has Id " + id);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id)
+        {
+            return Ok("Update on Id" + id);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteById(int id)
+        {
+            return Ok("Delete Id is" + id);
+        }
+        
+    }
+}
